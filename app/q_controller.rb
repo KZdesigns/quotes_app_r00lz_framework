03b2a24fdff933b0e31da3ff7b6aef0588b5996d
Kyle Zimmerman <kzdesignsios@gmail.com>
2025-02-16 20:29:42 -0800

    Adds shakes actions and view

--- app/q_controller.rb
+++ app/q_controller.rb
@@ -2,4 +2,9 @@ class QController < R00lz::Controller
     def a_quote
         "Testing if this worked: #{__dir__}"
     end
+
+    def shakes
+        @noun = :winking
+        render "shakes"
+    end
 end
