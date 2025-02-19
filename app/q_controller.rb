0bf39c1961545a1ca8bdee23096db2190f851d33
Kyle Zimmerman <kzdesignsios@gmail.com>
2025-02-18 19:33:36 -0800

    Adds card_trick, and fq actions, and adds quote view

--- app/q_controller.rb
+++ app/q_controller.rb
@@ -7,4 +7,14 @@ class QController < R00lz::Controller
         @noun = :winking
         render "shakes"
     end
+
+    def card_trick
+        n = params["card"] || "Queen"
+        "Your card is the #{n} of spades!"
+    end
+
+    def fq
+        @q = FileModel.find(params["q"] || 1)
+        render :quote
+    end
 end
